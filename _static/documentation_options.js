0c2a77db0ad7f8299d17b44067abc7b643464594
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2024-07-26 20:43:18 +0000

    [doc] build from commit b3f3e1f64ccf4392983649f75e514f0768032475

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2024.7.26+b3f3e1f64',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
